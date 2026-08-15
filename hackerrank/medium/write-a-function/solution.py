ed3025556a71500c4df5a18e32ebe37882022478
Preetham K P <pp9687365@gmail.com>
2026-08-15 19:37:41 +0530

    ✅ Write a function | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-15

--- hackerrank/medium/write-a-function/solution.py
+++ hackerrank/medium/write-a-function/solution.py
@@ -1,18 +1,8 @@
-#!/bin/python3
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        leap = True
+    return leap
 
-import math
-import os
-import random
-import re
-import sys
-
-
-
-if __name__ == '__main__':
-    n = int(input().strip())
-
-    if n % 2 != 0 or 6<=n<=20:
-        print("Weird")
-    else:
-        if 2<=n<=5 or n>20: 
-            print("Not Weird")
