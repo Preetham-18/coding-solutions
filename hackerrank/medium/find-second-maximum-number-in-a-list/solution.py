7c6e3b4ca4caa4f561741ca2accc0987ac73ac8c
Preetham K P <pp9687365@gmail.com>
2026-08-28 17:51:32 +0530

    ✅ Find the Runner-Up Score! | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-28

--- hackerrank/medium/find-second-maximum-number-in-a-list/solution.py
+++ hackerrank/medium/find-second-maximum-number-in-a-list/solution.py
@@ -1,8 +1,5 @@
 if __name__ == '__main__':
-    x = int(input())
-    y = int(input())
-    z = int(input())
     n = int(input())
+    arr = map(int, input().split())
     
-    new_list = [[i,j,k]for i in range(0,x+1) for j in range(0,y+1) for k in range(0,z+1) if i+j+k != n ]
-    print(new_list)
+    print(sorted(list(set(arr)))[-2])
